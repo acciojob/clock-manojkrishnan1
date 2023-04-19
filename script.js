c8b4b924e6c3244fd3c693fc9a2b234e8aaaca9b
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-04-19 19:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const timer = document.getElementById('timer');
+function dynamicTime(){
+	const currentDate = new Date();
+    timer.innerText =  currentDate.getMonth()+'/'+currentDate.getDate()+'/'+currentDate.getFullYear()+', '+ currentDate.toLocaleTimeString().toUpperCase();
+        }
+        setInterval(dynamicTime, 1000);
